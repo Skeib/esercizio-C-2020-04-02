71358e75718bd041bf741c3014074b46ee80e9f9
utente <utente@debian10.units.it>
2020-04-16 17:48:30 +0200

    agg

--- esercizio-C-2020-04-02/esercizio-c-2020-04-02.c
+++ esercizio-C-2020-04-02/esercizio-c-2020-04-02.c
@@ -1,206 +1,172 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <errno.h> // errno
-#include <signal.h> // signal
+#include <string.h>
 
 #define NUM_CHARS 80
 
-//il programma legge caratteri da stdin.
+//il programma svolge le attività sotto descritte fino a quando incontra EOF su stdin.
+//
+//1: il programma legge caratteri da stdin.
+//Quando incontra EOF, il programma scrive quanti processi figli ha lanciato in tutto e poi termina.
+//
 //Il programma accumula i caratteri letti da stdin in un array di dimensione 80 chiamato char_array.
+//(introdurre nel codice un #define NUM_CHARS 80).
+//
 //Quando char_array è pieno, passa tutto l'array ad un nuovo processo figlio
 //che fa delle analisi che scrive su stdout (analisi sotto riportate).
 //
 //Dopo aver lanciato il processo figlio, il processo principale azzera char_array ed il programma continua da 1:
 //
+//
+//*analisi svolte dai processi figlio (sull'array di 80 caratteri ricevuto)*
+//carattere minimo (mostrare il primo minimo, si applica a chi risolve dal 3/4)
+//carattere massimo (mostrare il primo massimo, si applica a chi risolve dal 3/4)
+//carattere con più occorrenze (il primo, a parità di occorrenze, si applica a chi risolve dal 3/4)
+//carattere con meno occorrenze (il primo, a parità di occorrenze, si applica a chi risolve dal 3/4)
+//
 //fatta l'analisi e scritto i risultati , il processo figlio termina.
 
+char char_array[NUM_CHARS];
 
-typedef		//struttura definita per il calcolo dell'occorrenza dei termini
-		struct {
-			char carattere;
-			unsigned int occorrenze;
-			} char_int;
+void fork_creator();
+void analisi_figlio();
+unsigned int child_counter = 0;
 
-unsigned int occourence(char c, char * array){
+int main(int argc, char *argv[]){
 
-	int posto=0;
 
-	for(int i=0; i<NUM_CHARS; i++){
+	int c;
 
-		if(array[i] == c){
-			posto = i;
+	char * ptr;
+	char * end;
+
+	ptr = char_array;
+
+	end = &char_array[NUM_CHARS-1];
+
+	while((c=getchar())!=EOF){
+
+		*ptr = c;
+
+		if(ptr == end){
+			fork_creator();
+
+			child_counter++;
+			ptr = char_array;
+
+			memset(char_array, 0, NUM_CHARS*sizeof(char));
 		}
-	}
-	return posto;
-}
-
-char minimum(char * array){
-	//carattere minimo (mostrare il primo minimo)
-
-	unsigned int c;
-	c = (unsigned int)array[0];
-	char retr;
-
-	for(int i=0; i<NUM_CHARS; i++){
-		unsigned int d = (unsigned int)array[i];
-
-		if(d < c){
-			c = d;
-			retr = array[i];
-		}
-	}
-	return retr;
-}
-
-
-char maximum(char * array){
-	//carattere massimo (mostrare il primo massimo)
-
-	unsigned int c;
-	c = (unsigned int)array[0];
-	char retr;
-
-	for(int i=0; i<NUM_CHARS; i++){
-		unsigned int d = array[i];
-
-		if(d>c){
-			c = d;
-			retr = array[i];
-		}
-	}
-	return retr;
-}
-
-int cercatore(char * array, char trova){
-
-	int prima_occorrenza = 0;
-	int i=0;
-
-	while(prima_occorrenza == 0){
-
-		if(array[i] == trova){
-			prima_occorrenza = i;
-		}else
-			i++;
-	}
-	return prima_occorrenza;
-}
-
-
-char_int occurence(char * char_occur, int num){
-
-	char_int char_arr[NUM_CHARS] = { 0 };
-
-	int count = 0;
-
-	//char_arr[0].carattere = char_occur[0];
-	//char_arr[0].occorrenze = 1;
-
-	for(int i=0; i<NUM_CHARS; i++){
-
-		count = 0;
-
-		while(count < NUM_CHARS){
-			if(char_arr[i].carattere == char_occur[count]){
-
-				char_arr[i].occorrenze++;
-				count++;
-			}
+		else{
+			ptr++;
 		}
 	}
 
-	char_int carattere = char_arr[0];
-
-	if(num==1){	//cerco il massimo
-	//carattere con più occorrenze (il primo, a parità di occorrenze)
-
-		for(int i=1; i<count; i++){
-
-			if(carattere.occorrenze < char_arr[i].occorrenze)
-				carattere = char_arr[i];
-
-		}
-	}
-	if(num ==0){//cerco il minimo
-//carattere con meno occorrenze (il primo, a parità di occorrenze)
-
-		for(int i=1; i<count; i++){
-
-			if(carattere.occorrenze > char_arr[i].occorrenze)
-				carattere = char_arr[i];
-
-		}
-	}
-	return carattere;
-}
-
-int main(){
-
-	char * char_array;
-	char c;
-
-	char_array = calloc(NUM_CHARS, sizeof(char));
-
-	unsigned int i=0;
-	unsigned int conta_figli =0;
-
-	while((c = getchar()) != EOF){
-
-		char_array[i] = c;
-
-		i++;
-		char min, max;
-		char_int occur_min;
-		char_int occur_max;
-
-
-
-		if(i == NUM_CHARS-1){
-
-			switch(fork()){
-
-			case 0:
-
-				min = minimum(char_array);
-				max = maximum(char_array);
-
-				occur_min = occurence(char_array, 0);
-
-				occur_max = occurence(char_array, 1);
-
-				printf("Il carattere minimo è %c ed occorre alla posizione: %u\n", min, cercatore(char_array, min));
-
-				printf("Il carattere massimo è %c ed occorre alla posizione: %u\n", max, cercatore(char_array, max));
-
-				printf("Il carattere che occorre più spesso è %c ed occorre alla posizione: %u\n",occur_max.carattere,occur_max.occorrenze);
-
-				printf("Il carattere che occorre meno spesso è %c ed occorre alla posizione: %u\n",occur_min.carattere, occur_min.occorrenze);
-
-				conta_figli++;
-				exit(0);
-				break;
-			case -1:
-				printf("fork() ha fallito! niente processo figlio!\n");
-
-				exit(1);
-				break;
-			default :
-				free(char_array);
-				char_array = calloc(NUM_CHARS, sizeof(char));
-				i=0;
-				break;
-			}
-
-		}
-	}
-
-//Quando incontra EOF, il programma scrive quanti processi figli ha lanciato in tutto e poi termina.
-
-	printf("il processo padre ha lanciato: %u processi figli", conta_figli);
+	printf("Il processo padre ha lanciato %u processi figli", child_counter);
 
 	return 0;
-
 }
+
+void fork_creator(){
+
+	switch(fork()){
+
+	case 0:
+		analisi_figlio();
+		break;
+	case -1:
+		perror("fork()");
+		exit(0);
+		break;
+	default:
+		//
+		break;
+
+	}
+}
+
+void analisi_figlio(){
+
+//*analisi svolte dai processi figlio (sull'array di 80 caratteri ricevuto)*
+//carattere minimo (mostrare il primo minimo)
+//carattere massimo (mostrare il primo massimo)
+//carattere con più occorrenze (il primo, a parità di occorrenze)
+//carattere con meno occorrenze (il primo, a parità di occorrenze)
+
+
+	int minimo = 256;
+	int massimo = -1;
+
+	int car;
+
+	int pos_min;
+	int pos_max;
+
+	char char_occurrence[256] = { 0 };
+
+	for(int i =0; i<NUM_CHARS; i++){
+
+		car = (int)char_array[i] & 0xFF;
+
+		if(minimo > car){
+			minimo = car;
+			pos_min = i;
+		}
+		if(massimo < car){
+			massimo=car;
+			pos_max = i;
+		}
+
+		char_occurrence[car]++;
+
+	}
+
+	int freq_max = -1;
+	int freq_min = 81;
+
+	char car_fr_max;
+	char car_fr_min;
+
+	for(int i=0; i< 256; i++){
+
+		if(freq_max < char_occurrence[i]){
+
+			freq_max = char_occurrence[i];
+			car_fr_max = i;
+
+		}
+
+		if(freq_min > char_occurrence[i] && char_occurrence[i]>0){
+
+			freq_min = char_occurrence[i];
+			car_fr_min = i;
+
+		}
+	}
+	freq_max = 80;
+	freq_min = 80;
+
+	for(int i =0; i<NUM_CHARS; i++){
+
+		if(car_fr_max == char_array[i] && freq_max>i){
+			freq_max = i;
+		}
+
+		if(car_fr_min == char_array[i] && freq_min>i)
+			freq_min = i;
+
+	}
+
+
+	printf("Il carattere minimo è %c, ed occorre per la prima volta alla posizione: %d\n", minimo, pos_min);
+
+	printf("Il carattere massimo è %c, ed occorre per la prima volta alla posizione: %d\n", massimo, pos_max);
+
+	printf("Il carattere che compare con maggior frequenza è %c, ed occorre per la prima volta alla posizione: %d\n", car_fr_max, freq_max);
+
+	printf("Il carattere che compare con minor frequenza è %c, ed occorre per la prima volta alla posizione: %d\n", car_fr_min, freq_min);
+
+
+	exit(0);
+}
+
